1d651e4de598b3a347a6c6c090cca10e9393cce4
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-10-18 03:40:17 -0400

    1.0.0
    update basico

--- src/functions/createSheets.js
+++ src/functions/createSheets.js
@@ -1,33 +1,33 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 const sortingMethods = {
-  bubbleSort: 'Método da bolha',
-  selectionSort: 'Método de ordenação por seleção',
-  insertionSort: 'Método de ordenação por inserção',
-  mergeSort: 'Método de ordenação por intercalação',
-  quickSort: 'Método de ordenação por separação',
-  heapSort: 'Método de ordenação por monte'
+  bubbleSort: "Método da bolha",
+  selectionSort: "Método de ordenação por seleção",
+  insertionSort: "Método de ordenação por inserção",
+  mergeSort: "Método de ordenação por intercalação",
+  quickSort: "Método de ordenação rápida",
+  heapSort: "Método de ordenação por pilha",
 };
 
 const createSheets = () => {
-  let filePath = path.join(__dirname, '..', 'data', 'result.txt');
-  const runTimes = fs.readFileSync(filePath, 'utf-8').split('\n');
-  const header = [
-    'Método de ordenação',
-    ...['primeira', 'segunda', 'terceira', 'quarta', 'quinta'].map(value => `Tempo da ${value} execução (ms)`),
-    'Média aritmética'
-  ].map(value => `"${value}"`).join(',') + '\n';
+  let filePath = path.join(__dirname, "..", "data", "result.txt");
+  const runTimes = fs.readFileSync(filePath, "utf-8").split("\n");
+
+  const header = [ "Método de ordenação",...["primeira", "segunda", "terceira", "quarta", "quinta"].map((value) => `Tempo da ${value} execução (ms)`),"Média aritmética",].map((value) => `"${value}"`).join(",") + "\n";
   const map = new Map();
-  runTimes.forEach(runtime => {
-    const runtimeArray = runtime.split(' ');
+  runTimes.forEach((runtime) => {
+    const runtimeArray = runtime.split(" ");
     const sortingMethod = runtimeArray[0].slice(0, -1);
     const entry = Number(runtimeArray[3].slice(0, -1));
     const loop = Number(runtimeArray[6].slice(0, -1));
     const label = { sortingMethod, entry, loop };
     let value = 0;
     if (runtimeArray.length === 9) {
-      const [milliseconds, seconds, minutes, hours] = runtimeArray[7].split(/[:\.]/).reverse().map(value => Number(value));
+      const [milliseconds, seconds, minutes, hours] = runtimeArray[7]
+        .split(/[:\.]/)
+        .reverse()
+        .map((value) => Number(value));
       if (hours !== undefined) value += hours * 60 * 60 * 1000;
       value += minutes * 60 * 1000;
       value += seconds * 1000;
@@ -44,7 +44,11 @@ const createSheets = () => {
       let average = 0;
       sheets[i] += `"${sortingMethod}",`;
       for (let j = 1; j <= 5; ++j) {
-        const label = { sortingMethod: rawSortingMethod, entry: (i + 1), loop: j };
+        const label = {
+          sortingMethod: rawSortingMethod,
+          entry: i + 1,
+          loop: j,
+        };
         const value = map.get(JSON.stringify(label));
         sheets[i] += `"${value.toLocaleString()}",`;
         average += value;
@@ -52,9 +56,14 @@ const createSheets = () => {
       average /= 5;
       sheets[i] += `"${Number(average.toFixed(3)).toLocaleString()}"\n`;
     }
-    filePath = path.join(__dirname, '..', 'sheets', `results-over-entry-${(i + 1)}.csv`);
+    filePath = path.join(
+      __dirname,
+      "..",
+      "sheets",
+      `results-over-entry-${i + 1}.csv`
+    );
     fs.writeFileSync(filePath, sheets[i]);
   }
-}
+};
 
 createSheets();
